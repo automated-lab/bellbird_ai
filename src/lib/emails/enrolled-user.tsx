d0924d1ce15f1a711fc6ad8fbc374baf88496fad
AissaSemaoui <semaoui0347@gmail.com>
2024-02-08 11:38:48 +0100

    try fix enrolled user email bug

--- src/lib/emails/enrolled-user.tsx
+++ src/lib/emails/enrolled-user.tsx
@@ -30,10 +30,10 @@ export default function renderEnrollmentEmail(props: Props) {
 
   return render(
     <Html>
-      <Tailwind>
-        <Head />
-        <Preview>{previewText}</Preview>
+      <Head />
+      <Preview>{previewText}</Preview>
 
+      <Tailwind>
         <Body className="bg-gray-50 my-auto mx-auto font-sans">
           <Container className="border border-solid border-[#eaeaea] rounded-lg my-[40px] mx-auto p-[20px] w-[465px] bg-white">
             <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
